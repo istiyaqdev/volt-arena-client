b621009289732c18591e0997a62212bd932f40fb
istiyaqdev <Istiyaqdev@gmail.com>
2026-05-31 23:33:30 +0600

    facilites page formatted

--- src/app/facilities/page.jsx
+++ src/app/facilities/page.jsx
@@ -1,12 +1,10 @@
 import AllFacilities from "@/Components/Facilities/AllFacilities";
 
 export const metadata = {
-    title: "VoltArena | All Facilities",
-    description: "Browse all elite sports facilities on VoltArena",
+  title: "VoltArena | All Facilities",
+  description: "Browse all elite sports facilities on VoltArena",
 };
 
-
-
 export default function Page() {
-    return <AllFacilities />;
+  return <AllFacilities />;
 }
