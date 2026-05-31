ab57c2b3f14481c791708d938178622788741f2d
istiyaqdev <Istiyaqdev@gmail.com>
2026-05-31 23:32:07 +0600

    formatted

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import Footer from "@/Components/Shared/Footer";
 import Navbar from "@/Components/Shared/Navbar";
+import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,13 +16,15 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "VoltArena",
-  description: "VoltArena is a premium, high-performance sports facility booking platform where you can instant-book fields, courts, and fitness arenas easily.",
+  description:
+    "VoltArena is a premium, high-performance sports facility booking platform where you can instant-book fields, courts, and fitness arenas easily.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en" data-theme="dark"
+      lang="en"
+      data-theme="dark"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
       <body className="min-h-full flex flex-col bg-(--green) text-(--text3) dark">
